63b47d89391505a1e20fa3e1eed38a62ba292a11
PedroSpiet <pedro.silva940@etec.sp.gov.br>
2020-03-14 18:30:03 -0300

    migration marmita

---
+++ database/migrations/2020_03_14_204306_marmitas.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Marmitas extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('Marmitas', function(Blueprint $table){
+            $table->bigIncrements('id');
+            $table->string('nome');
+            $table->longText('descricao');
+            $table->float('preco', 8,2);
+            $table->integer('estoque');
+            $table->string('image');
+            $table->integer('desconto')->nullable();
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('Marmitas', function(Blueprint $table){
+            $table->drop();
+        });
+    }
+}
